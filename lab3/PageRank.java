68a7250d983faac65c0ac9a599e7ed76daf98250
ldm <liudamingdl@gmail.com>
2022-10-18 15:44:57 -0400

    fix name

--- lab3/PageRank.java
+++ lab3/PageRank.java
@@ -8,19 +8,19 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class PageRank {
     public static void main(String[] args) throws Exception {
         if (args.length != 2) {
-            System.err.println("Usage: MaxTemperature <input path> <output path>");
+            System.err.println("Usage: PageRank <input path> <output path>");
             System.exit(-1);
         }
 //        Job job = new Job();
         // Job job = new Job();
         Job job = Job.getInstance();
-        job.setJarByClass(MaxTemperature.class);
+        job.setJarByClass(PageRank.class);
         job.setJobName("Max temperature");
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        job.setMapperClass(MaxTemperatureMapper.class);
-        job.setReducerClass(MaxTemperatureReducer.class);
-        job.setCombinerClass(MaxTemperatureReducer.class); //
+        job.setMapperClass(PageRankMapper.class);
+        job.setReducerClass(PageRankReducer.class);
+        job.setCombinerClass(PageRankReducer.class); //
         job.setNumReduceTasks(1); //
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
